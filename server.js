c7e15d452789d4a5bdb5e645f02ef020de4b1e30
Dimitri <65912918+dimitri12482@users.noreply.github.com>
2021-07-05 07:17:17 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const path = __dirname + '/app/views/';
+
+const app = express();
+
+
+app.use(express.static(path));
+
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+
+db.sequelize.sync();
+
+//built-in middleware function.
+
+app.get('/', function (req,res) {
+  res.sendFile(path + "index.html");
+});
+
+// // drop the table if it already exists
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
+
+require("./app/routes/turorial.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
